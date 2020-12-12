9cc1f423614800cae6a125cc39cfa16879956d57
gurkankirmaci <gurkankirmaci@gmail.com>
2020-12-12 00:00:31 +0000

    1.2.ve 4. soru yapıldı

--- odev.js
+++ odev.js
@@ -10,9 +10,8 @@ var person = {
   }
 }
 
-var messageFunc = person.message
+var messageFunc = person.message.bind(person);
 messageFunc();
-
 
 /*  
   Odev 2:
@@ -25,7 +24,7 @@ messageFunc();
 var numbers = {
   numbers: [[10,20,30], 2],
   multiply: function(){
-    this.numbers[0].map(function(number, numberIndex){
+    this.numbers[0].map((number, numberIndex) => {
         const result = number * this.numbers[1];
         console.log(result)
     })
@@ -58,8 +57,51 @@ function isValidName(name){
   Ornek: katilimSaati("3", 20) 60 sonucunu vermelidir.
   Ornek: katilimSaati("5", "30") 150 sonucunu vermelidir.
 */
-function katilimSaati(dersSayisi, dersSuresi){
 
-}
+function katilimSaati(dersSayisi, dersSuresi)
+{
 
+    if (typeof (dersSayisi) == "string" || typeof (dersSuresi) == "string" || typeof (dersSayisi) == "number" || typeof (dersSuresi) == "number")
+    {
+        if (dersSayisi == "" || dersSuresi == "")
+        {
 
+            return false;
+
+        }
+        else if (parseInt(dersSayisi) && parseInt(dersSuresi))
+        {
+            return Number(dersSayisi) * Number(dersSuresi);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+};
+    
+    console.log(!!katilimSaati(6, 10) );
+    console.log(!!katilimSaati(6, "10") );
+    console.log(!!katilimSaati("6", 10));
+    console.log(!!katilimSaati("6", "10"));
+    console.log(katilimSaati("", 6) );
+    console.log(katilimSaati(6, "") );
+    console.log(katilimSaati("", "") );
+    console.log(katilimSaati("foo", 6));
+    console.log(katilimSaati(6, "foo") );
+    console.log(katilimSaati("foo", "bar") );
+    console.log(katilimSaati(null, null) );
+    console.log(katilimSaati(null, undefined));
+    console.log(katilimSaati(undefined, null) );
+    console.log(katilimSaati(undefined, undefined) );
+    console.log(katilimSaati(Infinity, Infinity) );
+    console.log(katilimSaati(undefined, Infinity) );
+    console.log(katilimSaati(Infinity, undefined) );
+    console.log(katilimSaati(false, false) );
+    console.log(katilimSaati(false, true) );
+    console.log(katilimSaati(true, false) );
+    console.log(katilimSaati(true, true));
